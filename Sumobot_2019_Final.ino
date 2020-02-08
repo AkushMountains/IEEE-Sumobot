802ebc4fc76657d4f9417bf364c57423b474a2f3
AkushMountains <25161277+AkushMountains@users.noreply.github.com>
2020-02-08 15:58:22 -0500

    Code with Comments added

--- Sumobot_2019_Final.ino
+++ Sumobot_2019_Final.ino
@@ -1,18 +1,14 @@
 #include <Zumo32U4.h>
-
 Zumo32U4LCD lcd;
 Zumo32U4ProximitySensors proxsensors;
 Zumo32U4LineSensors lineSensors;
 Zumo32U4Motors motors;
 Zumo32U4ButtonA bA;
-
 #define RIGHT 1
 #define LEFT 0
 #define NUM_SENSORS 5
 uint16_t lineSensorValues[NUM_SENSORS];
 uint8_t selectedSensorIndex = 0;
-
-
 void setup() {
     lcd.clear();
     lcd.print(F("Let's get ready to rumble"));
@@ -39,14 +35,12 @@ void loadCustomCharacters()
   lcd.loadCustomCharacter(levels + 5, 5);  // 6 bars
   lcd.loadCustomCharacter(levels + 6, 6);  // 7 bars
 }
-
 void printBar(uint8_t height)
 {
   if (height > 8) { height = 8; }
   const char barChars[] = {' ', 0, 1, 2, 3, 4, 5, 6, 255};
   lcd.print(barChars[height]);
 }
-
 void printReadingsToLCD()
 {
   // On the first line of the LCD, display the bar graph.
@@ -63,7 +57,6 @@ void printReadingsToLCD()
   lcd.print(lineSensorValues[selectedSensorIndex]);
   lcd.print(F("    "));
 }
-
 // Prints a line with all the sensor readings to the serial
 // monitor.
 void printReadingsToSerial()
@@ -90,14 +83,12 @@ void linesensors()
   }
 }
 }
-
 void loop() {
     proxsensors.read();
     int left = proxsensors.countsLeftWithLeftLeds();
     int center_left = proxsensors.countsFrontWithLeftLeds();
     int center_right = proxsensors.countsFrontWithRightLeds();
     int right = proxsensors.countsRightWithRightLeds();
-
 // prints the values of the prox sensors on the LCD screen
     lcd.gotoXY(0,1);
     lcd.print(left);
@@ -108,51 +99,70 @@ void loop() {
     lcd.print(" ");
     lcd.print(right);
     lcd.print(" ");
-
     static uint16_t lastSampleTime = 0;
-
     if ((uint16_t)(millis() - lastSampleTime) >= 100)
      {
         lastSampleTime = millis();
-
         // Read the line sensors.
         lineSensors.read(lineSensorValues);
-
         // Send the results to the LCD and to the serial monitor.
         printReadingsToLCD();
         printReadingsToSerial();
-      }  
+      }
     linesensors();
+    // Comments on how to write the body of the code
 
+    //Line sensors read from 0 to 2000 based of wavelength of colr
+    // 0 = black
+    // ??? = white
+    // 2000= "air"
+    
+    //Proximity sensors read from 0 to 5
+    // closer you get to the Zumo bot, the numbers go higher
+    // DO NOT COVER THE SENSORS OTEHERWISE THE SENSOR WILL NOT READ CORRECTLY
+    // 5 = extremely close
+    // 0 = nothing in range
 
+    
+    // This is part of code to change for 2020
+    // incorporate line sensors and proximity sesnors to determine motor speeds
+
+    
+    // proximity sensors: seenleft, seenright,seenfront,attackfront return boolean value of True or False (look below for more info on this)
+    // motors: motors.setSpeeds(left,right) is used to change speed and direction of motors (max speed is +/- 400)
+    // line sensors: lineSensors.read(lineSensorValues) will read the linesensor values from 0 to 2000
+    // watch syntax for if and for loops
+
+    
+    // proximity sensors
+    // for these booleans change number values to what you want for closeness of opponent
     bool seenfront = center_left>=2 || center_right>=2; //this checks to see if the robot sees anyone in the front
     bool seenright = right>=2;
     bool seenleft = left>=2;
-    
     bool attackfront = center_left==5 || center_right==5;
+
+
     
-    if (seenfront) 
-    {
-        motors.setSpeeds(200,200);
-    }
-    else
-        if (seenright)
+    // line sensors
+    // can be used to reset motors if white line read
+    // this can be a little confusing so ask Akash or Gary if need help
+    // write if statments based on the booleans above
+    //example given below
+
+
+    
+   if (seenright)
         {
-            motors.setSpeeds(200,0);
+            motors.setSpeeds(50,0);
         }
-        else
-            if (seenleft)
+    else if (seenleft)
             {
-                motors.setSpeeds(0,200);
+                motors.setSpeeds(0,50);
             }
-            else
+    else
                 motors.setSpeeds(-100,100);
     if (attackfront)
     {
         motors.setSpeeds(250,250);
     }
-
-
-
-
-}
+    }
